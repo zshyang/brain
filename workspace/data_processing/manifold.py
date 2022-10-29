aab3fc7eef9cc5aa46819c079c9a9f360dd9d017
zshyang <zshyang1106@gmail.com>
2022-10-28 22:44:52 -0700

    file for make the mesh watertight

--- workspace/data_processing/manifold.py
+++ workspace/data_processing/manifold.py
@@ -1,9 +1,30 @@
-from glob import glob
 import os
+from glob import glob
 
+WT_PATH = 'manifold/manifold'
+
+def get_manifold_path(obj_file):
+    split_path = obj_file.split('/')
+    split_path[1] = 'manifold'
+    return '/'.join(split_path)
+
+
+def make_file_folder(file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+
+def generate_manifold_file(obj_file, manifold_path):
+    make_file_folder(manifold_path)
+    cmd = f'{WT_PATH} {obj_file} {manifold_path}'
+    os.system(cmd)
+    
 
 def main():
-    glob('../obj/')
+    list_obj_file = glob('../obj/*/l/*.obj')
+    for obj_file in list_obj_file:
+        manifold_path = get_manifold_path(obj_file)
+        generate_manifold_file(obj_file, manifold_path)
+    print(list_obj_file)
     pass
 
 
