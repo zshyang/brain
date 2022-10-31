42cb223bd4c8e9286b16f65cd748c5f6ab40ca6e
zshyang <zshyang1106@gmail.com>
2022-10-30 19:10:13 -0700

    remove comment

--- workspace/train/single_gpu_train.py
+++ workspace/train/single_gpu_train.py
@@ -16,11 +16,7 @@ def _fix_random(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    # for reproducibility
     torch.backends.cudnn.deterministic = True
-    # for speed
-    # since we are using minkowski engine, the network could
-    # not be speeded up with this option in my understanding
     torch.backends.cudnn.benchmark = False
 
 
