9d1539705fef95b22c5f47e8cc012bb7f0842e5f
zshyang <zshyang1106@gmail.com>
2022-11-02 21:07:48 -0700

    update test

--- workspace/train/single_gpu_test.py
+++ workspace/train/single_gpu_test.py
@@ -6,6 +6,27 @@ author:
 logs:
     20221101    file created
 '''
+import torch
+
+
+def parse_args():
+    pass
+
+
+def test():
+    # visit the dataset and get the features 
+
+    # partation the features 10 times to get the accuracy
+
+    pass
+
 
 if __name__ is '__main__':
-    
+    parse_args()
+    # set up the dataset and dataloader
+
+    # set up the network 
+    # load the pretrained weights
+
+    # do the test
+    test()
