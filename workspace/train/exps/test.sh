4b962f20cee6a937e5b4fa0be7184cec769ac5cd
zshyang <zshyang1106@gmail.com>
2022-11-03 17:46:29 -0700

    change from train to test

--- workspace/train/exps/test.sh
+++ workspace/train/exps/test.sh
@@ -1,4 +1,4 @@
-DEBUG=false
+DEBUG=true
 EXP_NAME="train"
 
 cd /workspace/$EXP_NAME
@@ -7,10 +7,10 @@ if $DEBUG
 then
     CUDA_VISIBLE_DEVICES=0 python \
     -m debugpy --listen 0.0.0.0:5566 --wait-for-client \
-    single_gpu_train.py \
-    --options exps/train.yaml
+    single_gpu_test.py \
+    --options exps/test.yaml
 else
     CUDA_VISIBLE_DEVICES=0 python \
-    single_gpu_train.py \
-    --options exps/train.yaml
+    single_gpu_test.py \
+    --options exps/test.yaml
 fi
