382afb6a4d842989f9c008e30cfb92f96ebcd241
zshyang <zshyang1106@gmail.com>
2022-11-05 10:44:10 -0700

    udpate

--- workspace/train/single_gpu_test.py
+++ workspace/train/single_gpu_test.py
@@ -6,25 +6,17 @@ author:
 logs:
     20221101    file created
 '''
-import torch
 import argparse
-from options import update_options
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-
-    str_help = 'experiment options file name'
-    parser.add_argument('--options',
-        help=str_help, required=True, type=str
-    )
-
-    args = parser.parse_args()
-
-    update_options(args.options)
+import torch
+from dataset import Dataset
+from model import Network
+from options import options, update_options
+from single_gpu_train import _fix_random, parse_args
 
 
 def test():
-    # visit the dataset and get the features 
+    # visit the dataset and get the features
 
     # partation the features 10 times to get the accuracy
 
@@ -33,10 +25,30 @@ def test():
 
 if __name__ is '__main__':
     parse_args()
-    # set up the dataset and dataloader
 
-    # set up the network 
+    # set up the dataset and dataloader
+    _fix_random(options.seed)
+
+    # make the dataset and dataloader
+    train_op = options.data.train
+    if train_op is not None:
+        train_dataset = Dataset(**train_op.dataset.params)
+        collate_fn = getattr(
+            train_dataset, str(train_op.dataloader.collate_fn), None
+        )
+        train_dataloader = torch.utils.data.DataLoader(
+            train_dataset, 
+            collate_fn=collate_fn,
+            batch_size=train_op.dataloader.batch_size,
+            drop_last=bool(train_op.dataloader.drop_last),
+            num_workers=int(train_op.dataloader.num_workers)
+        )
+
+    # set up the network
+    net = Network(**options.model.params).cuda()
     # load the pretrained weights
+    net.load_state_dict(torch.load(f'/runtime/300.pt')['net'])
+    net.eval()
 
     # do the test
     test()
