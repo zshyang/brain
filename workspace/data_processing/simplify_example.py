cc2ea1256f11dcd370d941ea6d74803e589f9c40
zshyang <zshyang1106@gmail.com>
2022-11-15 16:26:08 -0700

    update

---
+++ workspace/data_processing/simplify_example.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+
+from tqdm import tqdm
+
+SIMPLIFY_PATH = 'manifold/simplify'
+TARGET_FACE_NUM = 2048 * 4
+
+def get_path(obj_file):
+    split_path = obj_file.split('/')
+    split_path[1] = 'simplified'
+    return '/'.join(split_path)
+
+
+def make_file_folder(file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+
+def generate_file(obj_file, simplified_mesh_path):
+    make_file_folder(simplified_mesh_path)
+    cmd = f'{SIMPLIFY_PATH} -i {obj_file} -o {simplified_mesh_path} -m -f {TARGET_FACE_NUM}'
+    os.system(cmd)
+
+
+def main():
+    list_obj_file = glob('../manifold/*/l/*.obj')
+    for obj_file in tqdm(list_obj_file):
+        for res in [256, 512, 1024, 2048, 2048 * 2, 2048 * 4]:
+            cmd = f'{SIMPLIFY_PATH} -i {obj_file} -o {res}.obj -m -f {res}'
+            os.system(cmd)
+        break
+
+
+if __name__ == '__main__':
+    main()
