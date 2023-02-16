2ca69f1e30224a98d1b4d5f69c7b9e75c6fbc27c
zshyang <zshyang1106@gmail.com>
2023-02-16 00:15:28 -0700

    add some words

---
+++ src/data_processing/merge_dataset/1_/scripts/simg.sh
@@ -0,0 +1,19 @@
+DEBUG=$1
+INDEX=$2
+
+SIMG_DIR=/workspace/src/data_processing/merge_dataset/1_/
+SIMG_SCRIPTS_DIR=$SIMG_DIR"scripts/"
+
+echo $SIMG_SCRIPTS_DIR
+echo $SIMG_DIR
+
+if [ "$DEBUG" = "debug" ]; then
+    echo "Debug mode"
+else
+    echo $DEBUG" mode"
+fi
+
+singularity exec --no-home \
+--bind /scratch/zyang195/projects/brain/:/workspace/ \
+/scratch/zyang195/singularity/pytorch-1-12-1.simg \
+$SIMG_SCRIPTS_DIR/python.sh $DEBUG $INDEX $SIMG_DIR
