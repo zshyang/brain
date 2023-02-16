cece7a0c1bf54fae4c36e3622a7ba8a4132a2685
zshyang <zshyang1106@gmail.com>
2023-02-15 18:48:00 -0700

    add index as input args

--- src/data_processing/merge_dataset/scripts/debug_python.sh
+++ src/data_processing/merge_dataset/scripts/debug_python.sh
@@ -1,6 +1,9 @@
+index=$1
+
 # python used in singularity image
 cd /workspace/src/data_processing/merge_dataset
 
 python \
 -m debugpy --listen 0.0.0.0:5566 --wait-for-client \
-./merge.py
+./merge.py \
+--index $index
--- src/data_processing/merge_dataset/scripts/debug_simg.sh
+++ src/data_processing/merge_dataset/scripts/debug_simg.sh
@@ -1,4 +1,4 @@
 singularity exec --nv --no-home \
 --bind /scratch/zyang195/projects/brain/:/workspace/ \
 /scratch/zyang195/singularity/pytorch-1-12-1.simg \
-/workspace/src/data_processing/merge_dataset/scripts/debug_python.sh
+/workspace/src/data_processing/merge_dataset/scripts/debug_python.sh 2294
