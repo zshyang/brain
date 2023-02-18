de60d88c43b9b92d1842751f89c76bec62a2301d
zshyang <zshyang1106@gmail.com>
2023-02-17 23:54:09 -0700

    cha nge foler path

--- src/data_processing/merge_dataset/0_save_mfile_into_npz_json/scripts/debug_python.sh
+++ src/data_processing/merge_dataset/0_save_mfile_into_npz_json/scripts/debug_python.sh
@@ -1,7 +1,7 @@
 index=$1
 
 # python used in singularity image
-cd /workspace/src/data_processing/merge_dataset
+cd /workspace/src/data_processing/merge_dataset/0_save_mfile_into_npz_json
 
 python \
 -m debugpy --listen 0.0.0.0:5566 --wait-for-client \
