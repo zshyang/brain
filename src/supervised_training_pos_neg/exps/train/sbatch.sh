0a9f7476d503b5026d8abccf8a5f871c7804a494
zshyang <zshyang1106@gmail.com>
2023-02-23 22:23:39 -0700

    add ignore

--- src/supervised_training_pos_neg/exps/train/sbatch.sh
+++ src/supervised_training_pos_neg/exps/train/sbatch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #SBATCH -t 1-00:00:00 #upper bound time limit for job to finish
-#SBATCH -p htc
+#SBATCH -p general
 #SBATCH -q public
 #SBATCH --mem=100G
 #SBATCH --gres=gpu:1
 #SBATCH -o out_err/slurm.%A_%a.out
 #SBATCH -e out_err/slurm.%A_%a.err
+#SBATCH --cpus-per-task=4
 
 ./simg.sh run 
